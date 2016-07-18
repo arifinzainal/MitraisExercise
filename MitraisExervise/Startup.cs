bd3f35b24077c1ba4786ff0770804e770c2fd894
arifinzainal <zainal.bangjay@gmail.com>
2016-07-18 16:21:52 +0700

    Add project files.

---
+++ MitraisExervise/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MitraisExervise.Startup))]
+namespace MitraisExervise
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MitraisExervise/WebServices/BODSService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="BODSService.asmx.cs" Class="MitraisExervise.WebServices.BODSService" %>
